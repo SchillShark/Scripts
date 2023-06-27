c43a4308957ac5a0e388f5ddb7878c5790d9d587
yellowsun <30481740+SchillShark@users.noreply.github.com>
2023-06-27 18:08:05 -0500

    Create setup.sh

---
+++ Setup/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install Github Desktop Application
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.1.1-linux1/GitHubDesktop-linux-3.1.1-linux1.deb
+sudo apt-get install gdebi-core 
+sudo gdebi GitHubDesktop-linux-3.1.1-linux1.deb
+
